144808d0d3da9240f97dfe4371dfb66eb6c4781d
Holger@Wunderland <holger_kremss@web.de>
2021-03-26 18:55:04 +0100

    fixed client path (thanx Fuchur!), removed main-test, doesn't work this way

--- www/main.js
+++ www/main.js
@@ -269,7 +269,7 @@ $(document).ready(function(){
 
   // websocket
 //  var sock = io.connect();
-  var sock = io.connect('', {path:'/client-test/socket.io'});
+  var sock = io.connect('', {path:'/client/socket.io'});
   sock.on('stream', function(buf){
     buf = doTelnetNegotions(sock, buf);
     writeServerData(buf);
