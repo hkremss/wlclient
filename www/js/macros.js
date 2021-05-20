d51025f69e32679aa366938361332bfa6b83956e
Holger@Wunderland <holger_kremss@web.de>
2021-05-20 18:48:40 +0200

    added more help on variables, substitutions, etc.

--- www/js/macros.js
+++ www/js/macros.js
@@ -789,8 +789,8 @@ var TMP;
                         ':steps to the right!\n' +
                         '\n' +
                         'You can execute a macro by typing \'/\' followed by the name of the macro. Macros can call ' +
-                        'other macros, but self recursion will be treated as error. You can define as much macros as ' +
-                        'you want, but the number of executed steps is arbitrary limited. Sorry.\n';
+                        'other macros, including itself, but recursion is limited to avoid eternal loops.\n\n' +
+                        'See: /list, /undef, substitution\n';
                     break;
                 case 'undef':
                     return '\n' +
@@ -799,7 +799,8 @@ var TMP;
                         'Usage: /undef &lt;name&gt;\n' +
                         '\n' +
                         'Undefines a named macro. No options provided. It is the counterpart to /def. I have no ' +
-                        'idea, why you would ever need it, but it exists. For you.\n';
+                        'idea, why you would ever need it, but it exists. For you.\n\n' +
+                        'See: /def, /list, substitution\n';
                     break;
                 case 'list':
                     return '\n' +
@@ -808,8 +809,15 @@ var TMP;
                         'Usage: /list [pattern]\n' +
                         '\n' +
                         'Lists all currently defined macros, sorted alphabetically. If [pattern] is provided, only ' +
-                        'macros with a matching name are listed. \n';
+                        'macros with a matching name are listed.\n\n' +
+                        'See: /def, /undef, substitution\n';
                     break;
+                case 'trigger':
+                case 'triggers': return '\n' +
+                    'Help on: triggers\n' +
+                    '\n' +
+                    'Soon!\n\n' +
+                    'See: /def, /undef, /list\n';
                 case 'set':
                     return '\n' +
                         'Help on: /set\n' +
@@ -820,7 +828,7 @@ var TMP;
                         'Sets the value of a globale variable (first syntax) or displays the value of a global ' +
                         'variable, if no value is provided. If no value and no name is provided is provided, all ' +
                         'global variables will be listed.\n\n' +
-                        'See: /listvar, /unset, /let\n';
+                        'See: /listvar, /unset, /let, variables\n';
                     break;
                 case 'unset':
                     return '\n' +
@@ -829,7 +837,7 @@ var TMP;
                         'Usage: /unset &lt;name&gt;\n' +
                         '\n' +
                         'Removes the value of a globale variable.\n\n' +
-                        'See: /listvar, /set, /let\n';
+                        'See: /listvar, /set, /let, variables\n';
                     break;
                 case 'listvar':
                     return '\n' +
@@ -839,7 +847,7 @@ var TMP;
                         '\n' +
                         'Lists global variables and displays their values. A glob pattern may be used to filter the ' +
                         'displayes variables.\n\n' +
-                        'See: /set, /unset, /let\n';
+                        'See: /set, /unset, /let, variables\n';
                     break;
                 case 'let':
                     return '\n' +
@@ -852,7 +860,36 @@ var TMP;
                         'and in child evaluation contexts. So it may be used for temporary data, while macros are ' +
                         'cascaded. Local variables shadow global variables and local variables, which were defined ' +
                         'in a higher context.\n\n' +
-                        'See: /listvar, /set, /unset\n';
+                        'See: /listvar, /set, /unset, variables\n';
+                    break;
+                case 'variables':
+                    return '\n' +
+                        'Help on: variables\n' +
+                        '\n' +
+                        'There are global variables, special global variables, local variables and macro parameters. ' +
+                        'Global variables may be defined by /set anytime and will be stored in the localStorage of ' +
+                        'your browser. Local variables are defined with /let and exist only during macro evaluation. ' +
+                        'Macro parameters may be used by substitution to pass arguments to macros. Special global ' +
+                        'variables have default values and usually can only be set to predefined values.\n' +
+                        'NOTE: Special global variables may be reserved in the future for special purpose settings.\n' +
+                        'Export your client settings to save your custom macros and variables permanently!\n\n' +
+                        'See: /listvar, /set, /unset, /let, subsitution\n';
+                    break;
+                case 'subsitution':
+                    return '\n' +
+                        'Help on: subsitution\n' +
+                        '\n' +
+                        'Macros may use global or local variables or parameters for substitute portions of the macro ' +
+                        'body. The substitution pattern has always the form (brackets are not optional!):\n\n' +
+                        ' %{selector}\n\n' +
+                        'The <i>selector</i> may be:\n' +
+                        ' name          - the name of a global or local variable, e.g. %{foo}\n' +
+                        ' #             - the the number of existing macro parameters\n' +
+                        ' 0             - the name of the current evaluated macro\n' +
+                        ' 1, 2, 3, etc. - positional macro parameters, e.g. %{1}\n' +
+                        '\n' +
+                        'More selectors may be available in the future.\n\n' +
+                        'See: /listvar, /set, /unset, /let, variables\n';
                     break;
                 default: return '\n' +
                     'Tiny Macro Processor V' + TMP.MacroProcessor.VERSION + '\n' +
@@ -865,13 +902,18 @@ var TMP;
                     'disappear completely in the future, without further notice.\n' +
                     '\n' +
                     'Commands:\n' +
-                    ' /def   - define a named macro\n' +
-                    ' /undef - undefine a named macro\n' +
-                    ' /list  - display a list of macros\n' +
+                    ' /def     - define a named macro\n' +
+                    ' /undef   - undefine a named macro\n' +
+                    ' /list    - display a list of macros\n' +
+                    ' /set     - set a global variable\n' +
+                    ' /unset   - unset a global variable\n' +
+                    ' /listvar - list all global variables, including special variables\n' +
+                    ' /let     - set the value of a local variable\n' +
                     ' /help &lt;command&gt; - help for any command (without \'/\')\n' +
                     '\n' +
-                    'The macros are stored in your browsers localStorage only and not saved permanently, ' +
-                    'yet! So try copy&paste of /list for now!\n';
+                    'The macros are stored in your browsers localStorage only. Export your client settings ' +
+                    'to save them permanently!\n\n' +
+                    'See: /def, /undef, /list, /set, /unset, /listvar, /let, variables, substitution';
             }
         };
         return MacroHelp;
--- www/src/macros_help.ts
+++ www/src/macros_help.ts
@@ -45,8 +45,8 @@ namespace TMP {
           ':steps to the right!\n'+
           '\n'+
           'You can execute a macro by typing \'/\' followed by the name of the macro. Macros can call '+
-          'other macros, but self recursion will be treated as error. You can define as much macros as '+
-          'you want, but the number of executed steps is arbitrary limited. Sorry.\n';
+          'other macros, including itself, but recursion is limited to avoid eternal loops.\n\n'+
+          'See: /list, /undef, substitution\n';
           break;
         case 'undef': return '\n'+
           'Help on: /undef\n'+
@@ -54,7 +54,8 @@ namespace TMP {
           'Usage: /undef &lt;name&gt;\n'+
           '\n'+
           'Undefines a named macro. No options provided. It is the counterpart to /def. I have no '+
-          'idea, why you would ever need it, but it exists. For you.\n';
+          'idea, why you would ever need it, but it exists. For you.\n\n'+
+          'See: /def, /list, substitution\n';
           break;
         case 'list': return '\n'+
           'Help on: /list\n'+
@@ -62,8 +63,14 @@ namespace TMP {
           'Usage: /list [pattern]\n'+
           '\n'+
           'Lists all currently defined macros, sorted alphabetically. If [pattern] is provided, only '+
-          'macros with a matching name are listed. \n';
+          'macros with a matching name are listed.\n\n'+
+          'See: /def, /undef, substitution\n';
           break;
+        case 'trigger': case 'triggers': return '\n'+
+          'Help on: triggers\n'+
+          '\n'+
+          'Soon!\n\n'+
+          'See: /def, /undef, /list\n';
         case 'set': return '\n'+
           'Help on: /set\n'+
           '\n'+
@@ -73,7 +80,7 @@ namespace TMP {
           'Sets the value of a globale variable (first syntax) or displays the value of a global '+
           'variable, if no value is provided. If no value and no name is provided is provided, all '+
           'global variables will be listed.\n\n'+
-          'See: /listvar, /unset, /let\n';
+          'See: /listvar, /unset, /let, variables\n';
           break;
         case 'unset': return '\n'+
           'Help on: /unset\n'+
@@ -81,7 +88,7 @@ namespace TMP {
           'Usage: /unset &lt;name&gt;\n'+
           '\n'+
           'Removes the value of a globale variable.\n\n'+
-          'See: /listvar, /set, /let\n';
+          'See: /listvar, /set, /let, variables\n';
           break;
         case 'listvar': return '\n'+
           'Help on: /listvar\n'+
@@ -90,7 +97,7 @@ namespace TMP {
           '\n'+
           'Lists global variables and displays their values. A glob pattern may be used to filter the '+
           'displayes variables.\n\n'+
-          'See: /set, /unset, /let\n';
+          'See: /set, /unset, /let, variables\n';
           break;
         case 'let': return '\n'+
           'Help on: /let\n'+
@@ -102,7 +109,34 @@ namespace TMP {
           'and in child evaluation contexts. So it may be used for temporary data, while macros are '+
           'cascaded. Local variables shadow global variables and local variables, which were defined '+
           'in a higher context.\n\n'+
-          'See: /listvar, /set, /unset\n';
+          'See: /listvar, /set, /unset, variables\n';
+          break;
+        case 'variables': return '\n'+
+          'Help on: variables\n'+
+          '\n'+
+          'There are global variables, special global variables, local variables and macro parameters. '+
+          'Global variables may be defined by /set anytime and will be stored in the localStorage of '+
+          'your browser. Local variables are defined with /let and exist only during macro evaluation. '+
+          'Macro parameters may be used by substitution to pass arguments to macros. Special global '+
+          'variables have default values and usually can only be set to predefined values.\n'+
+          'NOTE: Special global variables may be reserved in the future for special purpose settings.\n'+
+          'Export your client settings to save your custom macros and variables permanently!\n\n'+
+          'See: /listvar, /set, /unset, /let, subsitution\n';
+          break;
+        case 'subsitution': return '\n'+
+          'Help on: subsitution\n'+
+          '\n'+
+          'Macros may use global or local variables or parameters for substitute portions of the macro '+
+          'body. The substitution pattern has always the form (brackets are not optional!):\n\n'+
+          ' %{selector}\n\n'+
+          'The <i>selector</i> may be:\n'+
+          ' name          - the name of a global or local variable, e.g. %{foo}\n'+
+          ' #             - the the number of existing macro parameters\n'+
+          ' 0             - the name of the current evaluated macro\n'+
+          ' 1, 2, 3, etc. - positional macro parameters, e.g. %{1}\n'+
+          '\n'+
+          'More selectors may be available in the future.\n\n'+
+          'See: /listvar, /set, /unset, /let, variables\n';
           break;
         default: return '\n'+
           'Tiny Macro Processor V' + MacroProcessor.VERSION + '\n'+
@@ -115,13 +149,18 @@ namespace TMP {
           'disappear completely in the future, without further notice.\n'+
           '\n'+
           'Commands:\n'+
-          ' /def   - define a named macro\n'+
-          ' /undef - undefine a named macro\n'+
-          ' /list  - display a list of macros\n'+
+          ' /def     - define a named macro\n'+
+          ' /undef   - undefine a named macro\n'+
+          ' /list    - display a list of macros\n'+
+          ' /set     - set a global variable\n'+
+          ' /unset   - unset a global variable\n'+
+          ' /listvar - list all global variables, including special variables\n'+
+          ' /let     - set the value of a local variable\n'+
           ' /help &lt;command&gt; - help for any command (without \'/\')\n'+
           '\n'+
-          'The macros are stored in your browsers localStorage only and not saved permanently, '+
-          'yet! So try copy&paste of /list for now!\n';
+          'The macros are stored in your browsers localStorage only. Export your client settings '+
+          'to save them permanently!\n\n'+
+          'See: /def, /undef, /list, /set, /unset, /listvar, /let, variables, substitution';
       }
     }
   }
